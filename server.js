bd0569e8a0b971ceb263ba44be9f6f5131c621c0
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-02-20 17:54:21 -0600

    testing new branch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var paypal = require('paypal-rest-sdk');
+
 
 
 //creating cookies and underline session
