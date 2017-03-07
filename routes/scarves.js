eadcf9995b010bdb9e0875e4149d3fc4a839ee30
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-03-07 09:30:56 -0600

    added more products

--- routes/scarves.js
+++ routes/scarves.js
@@ -135,7 +135,6 @@ router.post('/', function(req, res){
     } else {
 
       client.query('INSERT INTO cart (name, color, description, type, material, image_url, price, qty, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7,$8 , $9) RETURNING *;',
-      //has to be the same as object names inside of gif-service
       [req.body.name, req.body.color, req.body.description, req.body.type, req.body.material, req.body.image_url, req.body.price, 1, req.user.id],
       function(err, result){
         //waiting for database to get information back
